62d3dcd99c8ce98d892a847eb3dd245268f223e9
Soburr <adebesinbenzenejr99@gmail.com>
2025-11-05 08:35:37 +0100

    updates

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\Admin::firstOrCreate(
+            ['email' => 'admin@lagartisan.com'],
+            [
+                'name' => 'Super Admin',
+                'password' => Hash::make('password'),
+                'role' => 'super_admin',
+                'status' => true
+            ]
+        );
+    }
+}
