bf55bf9739be2452960f1cd1928957205b84369a
Soburr <adebesinbenzenejr99@gmail.com>
2026-06-18 22:22:34 +0100

    feat/post

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -9,10 +9,10 @@
             </p>
             <!-- Social Media -->
             <div class="flex mt-5 space-x-5">
-                <a href="#" aria-label="Twitter" class="text-blue-500 transition-colors duration-200 hover:text-blue-700"><i class="fab fa-twitter"></i></a>
-                <a href="#" aria-label="Facebook" class="text-blue-600 transition-colors duration-200 hover:text-blue-800"><i class="fab fa-facebook"></i></a>
-                <a href="#" aria-label="Instagram" class="text-pink-500 transition-colors duration-200 hover:text-pink-700"><i class="fab fa-instagram"></i></a>
-                <a href="#" aria-label="LinkedIn" class="text-purple-600 transition-colors duration-200 hover:text-purple-800"><i class="fab fa-linkedin"></i></a>
+                <!-- <a href="#" aria-label="Twitter" class="text-blue-500 transition-colors duration-200 hover:text-blue-700"><i class="fab fa-twitter"></i></a>
+                <a href="#" aria-label="Facebook" class="text-blue-600 transition-colors duration-200 hover:text-blue-800"><i class="fab fa-facebook"></i></a> -->
+                <a href="https://www.instagram.com/lagartisans?igsh=a2hkazVxa2kyb3k0" target="_blank" aria-label="Instagram" class="text-pink-500 transition-colors duration-200 hover:text-pink-700"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.linkedin.com/company/lag-artisans" target="_blank" aria-label="LinkedIn" class="text-purple-600 transition-colors duration-200 hover:text-purple-800"><i class="fab fa-linkedin"></i></a>
             </div>
         </div>
 
@@ -41,8 +41,8 @@
         <div>
             <h3 class="mb-4 text-lg font-semibold text-gray-900">Contact Us</h3>
             <ul class="space-y-3 text-sm">
-                <li>Email: <a href="mailto:support@unilagartisan.com" class="text-green-600 hover:underline">support@unilagartisan.com</a></li>
-                <li>Phone: <a href="tel:+2348123456789" class="text-green-600 hover:underline">+234 812 345 6789</a></li>
+                <li>Email: <a href="mailto:support@unilagartisan.com" class="text-green-600 hover:underline">lagartisans@gmail.com</a></li>
+                <li>Phone: <a href="tel:+2348123456789" class="text-green-600 hover:underline">+234 907 580 9425</a></li>
                 <li>Location: Akoka, Lagos</li>
             </ul>
         </div>
